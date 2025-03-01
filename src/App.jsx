5929d1921cb5a7c63f921036a09a1014b1d7eb93
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-03-01 23:44:45 +0530

    add route file to ethical page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/loginPage";
+import EthicalReportPage from "./pages/EthicalReportPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/ethicalReport" element={<EthicalReportPage />} />
       </Routes>
     </Router>
   );
