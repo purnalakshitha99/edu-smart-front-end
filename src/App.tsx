f61b9be77086687b82dc46870e1ffe1dcf3e7fd8
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-03-10 14:25:18 +0530

    chage the route file app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import Company from './pages/Company';
@@ -10,29 +9,36 @@ import ExamRoom from './pages/ExamRoom';
 import Footer from './components/Footer';
 import ClassRoom from './pages/ClassRoom';
 import ClassTime from './pages/ClassTime';
-import Login from './pages/Login';
-
+import AuthPage from './pages/AuthPage'; 
+// import PublicRoute from './components/PublicRoute';
+// import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/company" element={<Company />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/exams" element={<Exams />} />
-            <Route path="/class" element={<ClassTime/>} />  {/* Route to the Classroom PAGE */}
-            <Route path="/exam/:examId" element={<ExamRoom />} />
-            <Route path='/Classroom' element={<ClassRoom/>}/>
-          </Routes>
-        </main>
-        <Footer />
+
+        <Routes>
+          {/* Public Routes (Navbar/Footer for other pages) */}
+          <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
+          <Route path="/courses" element={<><Navbar /><Courses /><Footer /></>} />
+          <Route path="/company" element={<><Navbar /><Company /><Footer /></>} />
+          <Route path="/blog" element={<><Navbar /><Blog /><Footer /></>} />
+          <Route path="/about" element={<><Navbar /><AboutUs /><Footer /></>} />
+          <Route path="/exams" element={<><Navbar /><Exams /><Footer /></>} />
+          <Route path="/class" element={<><Navbar /><ClassTime /><Footer /></>} />
+          <Route path='/Classroom' element={<><Navbar /><ClassRoom /><Footer /></>} />
+
+          {/* Auth Page (No Navbar/Footer) - Renamed from Login to AuthPage*/}
+          <Route path="/login" element={<AuthPage />} />
+
+          {/* Exam Room (Example of a route that *could* have Navbar/Footer) - Adjust as needed.
+             The ExamRoom component itself would need to handle if it shows a footer or not. */}
+          <Route path="/exam/:examId" element={<><Navbar /><ExamRoom /><Footer /></>} />
+
+        </Routes>
+
       </div>
     </Router>
   );
