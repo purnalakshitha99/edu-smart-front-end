aa9dcf3c1f12d0ec33c27b0b4b07424bf43774ae
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-03-13 18:45:45 +0530

    ethical report page now change tsx and commit the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import LandingPage from "./pages/attemptQuiz/LandingPage";
-import LoginPage from "./pages/loginPage";
-import EthicalReportPage from "./pages/EthicalReportPage";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import MainPage from "./pages/MainPage";
+// import LandingPage from "./pages/attemptQuiz/LandingPage";
+// import LoginPage from "./pages/loginPage";
+// import EthicalReportPage from "./pages/EthicalReportPage";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/attempt-quiz" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/ethicalReport" element={<EthicalReportPage />} />
-      </Routes>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<MainPage />} />
+//         <Route path="/attempt-quiz" element={<LandingPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/ethicalReport" element={<EthicalReportPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
